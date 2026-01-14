5c04f39046468f3ec4c44efb21a1f6497f29cd53
Ahmed Abd ElFattah <ahmedabdelfattah3121@gmail.com>
2026-01-14 11:09:26 +0200

    First commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,14 +24,5 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
-
-        // Livewire fix for /public/ subdirectory
-        \Livewire\Livewire::setUpdateRoute(function ($handle) {
-            return Route::post('/public/livewire/update', $handle);
-        });
-
-        \Livewire\Livewire::setScriptRoute(function ($handle) {
-            return Route::get('/public/livewire/livewire.js', $handle);
-        });
     }
 }
